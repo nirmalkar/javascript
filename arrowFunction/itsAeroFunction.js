29e631f853989fc8807b8a2ddbd03dc74b0c363c
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-03 10:50:40 +0530

    arrow function

--- arrowFunction/itsAeroFunction.js
+++ arrowFunction/itsAeroFunction.js
@@ -1,3 +1,37 @@
-console.log(age);
-// const userName = "Hemant"
-var age = 27
+// aero function is not hoisted
+
+const obj = {
+    name:"hemant",
+    someMethod(){
+        this.name = "Hemant"
+        return this
+    },
+    arrowFunction:() => {
+    return this
+    }
+}
+
+function someMethod(){
+    this.name = "Hemant"
+    return this
+}
+
+const someMethod2 = () => {
+    this.name = "Hemant"
+    return this
+}
+
+
+// this one takes out the function then calls it
+const temp = obj.someMethod
+temp()
+//this one calls function with the reference of obj
+obj.someMethod() // output will be lexical env
+
+//output will be true because we are comparing the same function one is inside the object but called outside(global) and other is already at outside.
+console.log(temp() === someMethod());
+
+console.log(obj.someMethod());
+console.log(temp());
+console.log(someMethod());
+console.log(obj.arrowFunction());
---
+++ arrowFunction/questions.js
@@ -0,0 +1,14 @@
+console.log(op())
+// console.log(op1())
+
+function op(){
+    return "Hello world!"
+}
+
+const op1 = () => {
+    return "Hello parallel world!"
+}
+
+const sum = (a, b) => a + b
+
+console.log(sum(1, 2));
