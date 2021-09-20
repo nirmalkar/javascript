3ec1c83aba4937e0eacf143a84c737f9dade75ff
nirmalkar <hemantnirmalkar17@gmail.com>
2021-09-20 13:28:00 +0530

    config that i like on my vsc

--- arrowFunction/athis.js
+++ arrowFunction/athis.js
@@ -1,42 +1,41 @@
 // in node globalThis is just like window in browser
 const someObj = {
-    myFunction(){
-        console.log(this);
+    myFunction() {
+        console.log(this)
     },
-    myFunction2(){
-        const myObj2 ={
-            myFunction3(){
+    myFunction2() {
+        const myObj2 = {
+            myFunction3() {
                 console.log(this)
-            }
+            },
         }
-        
+
         return myObj2
     },
 }
 
 const test1 = someObj.myFunction
 // what would be the outputs
-test1() 
+test1()
 someObj.myFunction()
 
+const test2 = someObj.myFunction2
+console.log(test2().myFunction3())
+// // if any function is invoked without object it would be having global this
 
-// const test2 = someObj.myFunction2
-// console.log(test2().myFunction3());
-// // // if any function is invoked without object it would be having global this
+function myFunc() {
+    return this
+}
+const f = new myFunc()
+f.a = 10
+f.b = 20
+console.log(f)
 
-// function myFunc() {
-//     return this
-// }
-// const f = new myFunc()
-// f.a = 10
-// f.b = 20
-// console.log(f);
-
-// const obj ={
-//     simple(){
-//         return this
-//     }
-// }
-// console.log(obj.simple() === globalThis);
-// const t = obj.simple
-// console.log(t() === globalThis);
+const obj = {
+    simple() {
+        return this
+    },
+}
+console.log(obj.simple() === globalThis)
+const t = obj.simple
+console.log(t() === globalThis)
